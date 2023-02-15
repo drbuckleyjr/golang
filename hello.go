6dc72d076a2d4756f299de574b958df126121a14
Robert Buckley <rbuckley@ieee.org>
2023-02-15 14:16:30 -0800

    Add files via upload

--- hello.go
+++ hello.go
@@ -1,7 +1,26 @@
+// hello.go
+
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "log"
+    "Exper_3/greetings"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+    // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+    
+    // Request a greeting message.
+    message, err := greetings.Hello("Sam")
+    // If an error was returned, print it to the console and
+    // exit the program.
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(message)
 }
